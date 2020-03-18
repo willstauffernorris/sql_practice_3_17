66809a5405301723b930bcabb3b1a5d7bfc8f0c7
willsn <willsn@gmail.com>
2020-03-18 07:51:42 -0600

    inserting data

--- app/pg_queries.py
+++ app/pg_queries.py
@@ -9,18 +9,47 @@ DB_USER=os.getenv("DB_USER")
 DB_PASSWORD=os.getenv("DB_PASSWORD")
 DB_HOST=os.getenv("DB_HOST")
 
-
-### Connect to ElephantSQL-hosted PostgreSQL
 connection = psycopg2.connect(dbname=DB_NAME, user=DB_USER,
                         password=DB_PASSWORD, host=DB_HOST)
 print("CONNETION", connection)
-### A "cursor", a structure to iterate over db records to perform queries
+
+
 cursor = connection.cursor()
 print("CURSOR", cursor)
 
-### An example query
-cursor.execute('SELECT * from test_table;')
-### Note - nothing happened yet! We need to actually *fetch* from the cursor
+query = '''
+CREATE TABLE IF NOT EXISTS test_table (
+  id SERIAL PRIMARY KEY,
+  name varchar(40) NOT NULL,
+  data JSONB
+);
+'''
 
-result = cursor.fetchone()
-print("RESULT:", result)
+cursor.execute(query)
+
+cursor.execute('SELECT * from test_table;')
+result = cursor.fetchall()
+print("RESULT:", len(result))
+
+
+insertion_query = """
+INSERT INTO test_table (name, data) VALUES
+(
+  'A row name',
+  null
+),
+(
+  'Another row, with JSON',
+  '{ "a": 1, "b": ["dog", "cat", 42], "c": true }'::JSONB
+);
+"""
+
+cursor.execute(insertion_query)
+
+cursor.execute('SELECT * from test_table;')
+result = cursor.fetchall()
+print("RESULT:", len(result))
+
+
+##Save the transactions
+#connection.commit()
